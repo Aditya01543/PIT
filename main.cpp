56fa6b4eb85e541626d1fc9161f7b679312562c2
Aditya01543 <pjj8uruc9@gmail.com>
2026-06-13 17:46:10 +0530

    cat-file function working!!.

--- main.cpp
+++ main.cpp
@@ -64,6 +64,7 @@ int hashObject(fs::path filename){
                 cout<<hashedStr<<endl;
             }else{
                 cerr<<"Failed to open the hashed file lmaoooo SKILL ISSUE"<<endl<<"File : "<<objects / hashedStr.substr(0, 2) / hashedStr.substr(2, 38)<<endl;
+                objFile.close();
                 return 1;
             }
         }
@@ -75,8 +76,41 @@ int hashObject(fs::path filename){
     return 0;
 }
 
+int catFile(string hash){
+    if(hash.length() != 40){
+        cerr<<"Invalid hash."<<endl;
+        return 1;
+    }
+    if(fs::exists(objects/hash.substr(0, 2)/hash.substr(2, 38))){
+        ifstream hashFile(objects/hash.substr(0, 2)/hash.substr(2, 38), ios::binary);
+        
+        string content = "";
+
+        if(hashFile.is_open()){
+            char ch;
+            while(hashFile.get(ch)){
+                content+=ch;
+            }
+            hashFile.close();
+        }else{
+            cerr<<"Failed to open the object file for hash "<<hash<<endl;
+            hashFile.close();
+            return 1;
+        }
+
+        cout<<endl<<content<<endl;
+
+    }else{
+        cerr<<"The object with the hash "<<hash<<" does not exists."<<endl;
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     init();
     hashObject("README.md");
+    catFile("bf8e7f16dde9b13469f492478fc9990257f6fd58");
     return 0;
 }
