2ab4fc24c2a31a5526b036ec7d6152c342c8215b
Aditya01543 <pjj8uruc9@gmail.com>
2026-06-13 17:01:15 +0530

    hash-object function working!!.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 namespace fs = std::filesystem;
 
 fs::path dir = "C:/Aditya/Projects/lab-rat-folder";
+fs::path objects = dir / ".pit" / "objects";
 
 void init(){
     if(!fs::exists(dir / ".pit")){
@@ -27,7 +28,55 @@ void init(){
     }
 }
 
+int hashObject(fs::path filename){
+    if(fs::exists(dir / filename)){
+
+        ifstream file(dir / filename, ios::binary);
+
+        if(!file.is_open()){
+            cerr<<"Failed to open the file "<<dir/filename<<endl;
+            return 1;
+        }
+
+        string hashBuffer="";
+        char ch;
+        
+        while(file.get(ch)){
+            hashBuffer += ch;
+        }
+
+        file.close();
+
+        string hashedStr = sha1(hashBuffer);
+
+        if(!fs::exists(objects / hashedStr.substr(0, 2))){
+            fs::create_directory(objects / hashedStr.substr(0, 2));
+        }
+        
+        if(fs::exists(objects / hashedStr.substr(0, 2) / hashedStr.substr(2, 38))){
+            cout<<"File "<<dir/filename<<" already hashed"<<endl;
+        }else{
+            ofstream objFile(objects / hashedStr.substr(0, 2) / hashedStr.substr(2, 38));
+            if(objFile.is_open()){
+                objFile<<hashBuffer;
+                objFile.close();
+                cout<<"The file "<<dir/filename<<" hashed and stored with the hash :"<<endl;
+                cout<<hashedStr<<endl;
+            }else{
+                cerr<<"Failed to open the hashed file lmaoooo SKILL ISSUE"<<endl<<"File : "<<objects / hashedStr.substr(0, 2) / hashedStr.substr(2, 38)<<endl;
+                return 1;
+            }
+        }
+
+    }else{
+        cerr<< "File "<<dir/filename<<" does not exists"<<endl;
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     init();
+    hashObject("README.md");
     return 0;
 }
